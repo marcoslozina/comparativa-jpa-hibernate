350b74548fa303003c79962597dbaaf084ab9631
Marcos Lozina <marcoslozina@gmail.com>
2017-10-10 18:22:41 -0300

    Update ScriptCargaInicial.sql
    Actualizando script de carga de datos con propiedad identity

--- src/main/resources/META-INF/sql/ScriptCargaInicial.sql
+++ src/main/resources/META-INF/sql/ScriptCargaInicial.sql
@@ -1,13 +1,14 @@
-insert into bdcdt.CDT_persona (codigo, nombre) values (1, 'Martin Perez');
-insert into bdcdt.CDT_persona (codigo, nombre) values (2, 'Benito Sanchez');
-insert into bdcdt.CDT_persona (codigo, nombre) values (3, 'Paula Benitez');
-insert into bdcdt.CDT_persona (codigo, nombre) values (4, 'Maria Gonzales');
+insert into bd.CDT_persona (nombre) values ('Martin Perez');
+insert into bd.CDT_persona (nombre) values ('Benito Sanchez');
+insert into bd.CDT_persona (nombre) values ('Paula Benitez');
+insert into bd.CDT_persona (nombre) values ('Maria Gonzales');
 
-insert into bdcdt.CDT_registro (codigo_persona, descripcion, valor, tipoRegistro) values (1, 'Pago de servicio de luz', 1500.00, 'GASTO' );
-insert into bdcdt.CDT_registro (codigo_persona, descripcion, valor, tipoRegistro) values (1, 'Pago de servicio de agua', 500.00, 'GASTO' );
-insert into bdcdt.CDT_registro (codigo_persona, descripcion, valor, tipoRegistro) values (1, 'Salario', 21000.00, 'INGRESO' );
+insert into bd.CDT_registro (codigo_persona, descripcion, valor, tipoRegistro) values (1, 'Pago de servicio de luz', 1500.00, 'GASTO' );
+insert into bd.CDT_registro (codigo_persona, descripcion, valor, tipoRegistro) values (1, 'Pago de servicio de agua', 500.00, 'GASTO' );
+insert into bd.CDT_registro (codigo_persona, descripcion, valor, tipoRegistro) values (1, 'Salario', 21000.00, 'INGRESO' );
 
-insert into bdcdt.CDT_registro (codigo_persona, descripcion, valor, tipoRegistro) values (2, 'Pago de servicio de gaz', 250.00, 'GASTO' );
-insert into bdcdt.CDT_registro (codigo_persona, descripcion, valor, tipoRegistro) values (2, 'Pago de internet fija', 400.00, 'GASTO' );
-insert into bdcdt.CDT_registro (codigo_persona, descripcion, valor, tipoRegistro) values (2, 'Pago de internet 3g', 250.00, 'GASTO' );
+insert into bd.CDT_registro (codigo_persona, descripcion, valor, tipoRegistro) values (2, 'Pago de servicio de gaz', 250.00, 'GASTO' );
+insert into bd.CDT_registro (codigo_persona, descripcion, valor, tipoRegistro) values (2, 'Pago de internet fija', 400.00, 'GASTO' );
+insert into bd.CDT_registro (codigo_persona, descripcion, valor, tipoRegistro) values (2, 'Pago de internet 3g', 250.00, 'GASTO' );
+ 
  
