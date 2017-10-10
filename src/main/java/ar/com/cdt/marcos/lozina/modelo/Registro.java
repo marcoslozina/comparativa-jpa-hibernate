cc2b56405db369cb49483a2c8f766f77230900d6
Marcos Lozina <marcoslozina@gmail.com>
2017-10-10 18:28:20 -0300

    Update Registro.java
    agregando propiedad @GeneratedValue(strategy = GenerationType.IDENTITY)

--- src/main/java/ar/com/cdt/marcos/lozina/modelo/Registro.java
+++ src/main/java/ar/com/cdt/marcos/lozina/modelo/Registro.java
@@ -18,7 +18,7 @@ public class Registro implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigo;
 
     @ManyToOne
